275baa4c3c5f79369dc505a989aae5106b795b1b
Matthea Loo <>
2018-05-30 11:53:12 +0800

    new commit

--- app/src/main/java/com/example/firebaseauthdemo/ProfileActivity.java
+++ app/src/main/java/com/example/firebaseauthdemo/ProfileActivity.java
@@ -20,7 +20,8 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
 
-    // add comment 
+    // add comment
+    // add  comment1 
 
     private FirebaseAuth firebaseAuth;
 
